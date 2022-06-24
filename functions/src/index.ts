6d5fdf1697737937c7e046ae34ce87edf3bc13e0
ParagonTheDev <nipuna.sudha@gmail.com>
2022-06-25 03:42:29 +0530

    Upgrade cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,32 +6,32 @@ import {ApiUtils} from "./utils/api-utils";
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.status(200).send("Hello from Firebase!");
+exports.helloWorld = functions.region("europe-west1").https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!", {structuredData: true});
+    response.status(200).send("Hello from Firebase!");
 });
 
-exports.sendEmail = functions.firestore
+exports.sendEmail = functions.region("europe-west1").firestore
     .document("bookings/{bookingId}")
     .onCreate((snap, context) => {
-      if (snap.data().recaptchaToken!==null) {
-        // eslint-disable-next-line import/namespace,max-len
-        const result = ApiUtils.getRecaptchaStatus(snap.data().recaptchaToken).then(
-            (status)=>{
-              console.log("Get Recaptcha Token Verification Successfully ");
-              if (status) {
-                MailUtils.triggerEmail(snap);
-              } else {
-                DBUtils.deleteBooking(snap).then(()=>{
-                  console.log("Deleted Spam Record..");
-                });
-              }
-            }
-        );
-        console.log("Result is : "+result);
-      } else {
-        DBUtils.deleteBooking(snap).then(()=>{
-          console.log("Deleted Unauthorized/Spam Record..");
-        });
-      }
+        if (snap.data().recaptchaToken !== null) {
+            // eslint-disable-next-line import/namespace,max-len
+            const result = ApiUtils.getRecaptchaStatus(snap.data().recaptchaToken).then(
+                (status) => {
+                    console.log("Get Recaptcha Token Verification Successfully ");
+                    if (status) {
+                        MailUtils.triggerEmail(snap);
+                    } else {
+                        DBUtils.deleteBooking(snap).then(() => {
+                            console.log("Deleted Spam Record..");
+                        });
+                    }
+                }
+            );
+            console.log("Result is : " + result);
+        } else {
+            DBUtils.deleteBooking(snap).then(() => {
+                console.log("Deleted Unauthorized/Spam Record..");
+            });
+        }
     });
