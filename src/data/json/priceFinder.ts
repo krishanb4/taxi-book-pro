aa6c22ce6d4fd4915de954f2d48ba0b6705fc8dd
KMuthumala <lgkmuthumala@gmail.com>
2022-06-28 05:19:34 +0530

    Add two-way search destinations

--- src/data/json/priceFinder.ts
+++ src/data/json/priceFinder.ts
@@ -4,8 +4,13 @@ import data2 from "./all-trips.json"
 import * as _ from "lodash";
 import {BookingService} from "../../services/booking-service";
 
+function getPersonVerification(personalDetails: PersonalDetails) {
+    return personalDetails.getAdultCount() + personalDetails.getChildCount() === 1 || personalDetails.getAdultCount() + personalDetails.getChildCount() === 2 || personalDetails.getAdultCount() + personalDetails.getChildCount() === 3;
+}
+
 function findPrice(bookingDetails: BookingDetails, personalDetails: PersonalDetails, bookingService: BookingService): any {
     let trip: string;
+    let selectedPrice:any;
     if (bookingService.getJourneyType() === JourneyType.ARRIVAL_ONE_WAY) {
         trip = "ONE WAY";
     } else if (bookingService.getJourneyType() === JourneyType.DEPARTURE) {
@@ -17,11 +22,24 @@ function findPrice(bookingDetails: BookingDetails, personalDetails: PersonalDeta
     let selected : {list: Record<string, any>} | undefined = _.find(data2, function (value, key, collection) {
         return value.trip === trip && value.from === bookingDetails.getPickUpPoint() && value.to === bookingDetails.getDropPoint();
     });
-    if (personalDetails.getAdultCount() + personalDetails.getChildCount() === 1 || personalDetails.getAdultCount() + personalDetails.getChildCount() === 2 || personalDetails.getAdultCount() + personalDetails.getChildCount() === 3) {
-        return selected?.list["1-3 P."];
+    if (getPersonVerification(personalDetails)) {
+        selectedPrice =  selected?.list["1-3 P."];
     } else {
-        return selected?.list[(personalDetails.getAdultCount() + personalDetails.getChildCount()).toString() + " P."];
+        selectedPrice =  selected?.list[(personalDetails.getAdultCount() + personalDetails.getChildCount()).toString() + " P."];
     }
+
+    if(!selectedPrice){
+        selected  = _.find(data2, function (value, key, collection) {
+            return value.trip === trip && value.to === bookingDetails.getPickUpPoint() && value.from === bookingDetails.getDropPoint();
+        });
+        if (getPersonVerification(personalDetails)) {
+            selectedPrice =  selected?.list["1-3 P."];
+        } else {
+            selectedPrice =  selected?.list[(personalDetails.getAdultCount() + personalDetails.getChildCount()).toString() + " P."];
+        }
+    }
+
+    return selectedPrice;
 }
 
 export {findPrice};
