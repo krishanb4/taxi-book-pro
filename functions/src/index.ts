91b97125e66315f4ab87d999d192b1179b11d5ab
ParagonTheDev <nipuna.sudha@gmail.com>
2022-06-29 04:09:05 +0530

    Fix email generation issues

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,37 +1,22 @@
 import * as functions from "firebase-functions";
-// eslint-disable-next-line import/default
 import {MailUtils} from "./utils/mail-utils";
 import {DBUtils} from "./utils/db-utils";
 import {ApiUtils} from "./utils/api-utils";
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
-
-exports.helloWorld = functions.region("europe-west1").https.onRequest((request, response) => {
-    functions.logger.info("Hello logs!", {structuredData: true});
-    response.status(200).send("Hello from Firebase!");
-});
 
 exports.sendEmail = functions.region("europe-west1").firestore
     .document("bookings/{bookingId}")
     .onCreate(async (snap, context) => {
         if (snap.data().recaptchaToken !== null) {
             // eslint-disable-next-line import/namespace,max-len
-            const result = ApiUtils.getRecaptchaStatus(snap.data().recaptchaToken).then(
-                async (status) => {
-                    console.log("Get Recaptcha Token Verification Successfully ");
-                    if (status) {
-                        await MailUtils.triggerEmail(snap);
-                    } else {
-                        DBUtils.deleteBooking(snap).then(() => {
-                            console.log("Deleted Spam Record..");
-                        });
-                    }
-                }
-            );
-            console.log("Result is : " + result);
+            const captchaSuccess = await ApiUtils.getRecaptchaStatus(snap.data().recaptchaToken);
+            console.log(`Get Recaptcha verification: ${captchaSuccess}`);
+            if (captchaSuccess) {
+                await MailUtils.triggerEmail(snap);
+            }
+            console.log(`Email trigger complete for: ${snap.id}`);
         } else {
             DBUtils.deleteBooking(snap).then(() => {
-                console.log("Deleted Unauthorized/Spam Record..");
+                console.log("Spam order detected.");
             });
         }
     });
--- functions/src/utils/data-extract-utils.ts
+++ functions/src/utils/data-extract-utils.ts
@@ -1,81 +1,80 @@
 // eslint-disable-next-line require-jsdoc
 import {QueryDocumentSnapshot} from "firebase-functions/lib/providers/firestore";
-// eslint-disable-next-line require-jsdoc
-export class DataExtractUtils{
 
-    public static getTripId(snap:QueryDocumentSnapshot):string{
+// eslint-disable-next-line require-jsdoc
+export class DataExtractUtils {
+
+    public static getTripId(snap: QueryDocumentSnapshot): string {
         return snap.id;
     }
 
-    public static getTrip(snap:QueryDocumentSnapshot):string{
-        if(snap.data().journeyType === 0){
+    public static getTrip(snap: QueryDocumentSnapshot): string {
+        if (snap.data().journeyType === 0) {
             return "Arrival";
-        }else if(snap.data().journeyType === 1){
+        } else if (snap.data().journeyType === 1) {
             return "Round Trip";
-        }else{
+        } else {
             return "Departure";
         }
     }
 
-    public static getCost(snap:QueryDocumentSnapshot):number{
-        if(snap.data().journeyType === 0){
+    public static getCost(snap: QueryDocumentSnapshot): number {
+        if (snap.data().journeyType === 0) {
             return snap.data().arrival.cost;
-        }else if(snap.data().journeyType === 1){
+        } else if (snap.data().journeyType === 1) {
             return snap.data().arrival.cost;
-        }else{
+        } else {
             return snap.data().departure.cost;
         }
     }
 
-    public static getCustomerName(snap:QueryDocumentSnapshot):string{
+    public static getCustomerName(snap: QueryDocumentSnapshot): string {
         return snap.data().personalDetails.name;
     }
 
-    public static getPickupPoint(snap:QueryDocumentSnapshot):string{
-        if(snap.data().journeyType === 0){
+    public static getPickupPoint(snap: QueryDocumentSnapshot): string {
+        if (snap.data().journeyType === 0) {
             return snap.data().arrival.pickUpPoint;
-        }else if(snap.data().journeyType === 1){
+        } else if (snap.data().journeyType === 1) {
             return snap.data().arrival.pickUpPoint;
-        }else{
+        } else {
             return snap.data().departure.pickUpPoint;
         }
     }
 
-    public static getDestinationPoint(snap:QueryDocumentSnapshot):string{
-        if(snap.data().journeyType === 0){
+    public static getDestinationPoint(snap: QueryDocumentSnapshot): string {
+        if (snap.data().journeyType === 0) {
             return snap.data().arrival.dropPoint;
-        }else if(snap.data().journeyType === 1){
+        } else if (snap.data().journeyType === 1) {
             return snap.data().arrival.dropPoint;
-        }else{
+        } else {
             return snap.data().departure.dropPoint;
         }
     }
 
-    public static getPersonCount(snap:QueryDocumentSnapshot):number{
+    public static getPersonCount(snap: QueryDocumentSnapshot): number {
         return snap.data().personalDetails.adultCount + snap.data().personalDetails.childCount;
     }
 
-    public static getBookedDate(snap:QueryDocumentSnapshot):Date{
-        return snap.data().personalDetails.created.toDate();
+    public static getBookedDate(snap: QueryDocumentSnapshot): string {
+        return snap.data().created.toString();
     }
 
-    public static getPhone(snap:QueryDocumentSnapshot):string{
+    public static getPhone(snap: QueryDocumentSnapshot): string {
         return snap.data().personalDetails.phone;
     }
 
-    public static getEmail(snap:QueryDocumentSnapshot):string{
+    public static getEmail(snap: QueryDocumentSnapshot): string {
         return snap.data().personalDetails.email;
     }
 
-    public static getTripDate(snap:QueryDocumentSnapshot):Date{
-        if(snap.data().journeyType === 0){
-            return snap.data().arrival.pickUpDate.toDate();
-        }else if(snap.data().journeyType === 1){
-            return snap.data().arrival.pickUpDate.toDate();
-        }else{
-            return snap.data().departure.pickUpDate.toDate();
+    public static getTripDate(snap: QueryDocumentSnapshot): string {
+        if (snap.data().journeyType === 0) {
+            return snap.data().arrival.pickUpDate;
+        } else if (snap.data().journeyType === 1) {
+            return snap.data().arrival.pickUpDate;
+        } else {
+            return snap.data().departure.pickUpDate;
         }
     }
-
-
 }
