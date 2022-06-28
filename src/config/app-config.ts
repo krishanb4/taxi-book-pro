8dfe1deb8fa26ce6fad6f5be09eb2d29c1234f68
ParagonTheDev <nipuna.sudha@gmail.com>
2022-06-29 04:45:51 +0530

    Enable production

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -1,6 +1,6 @@
 export class AppConfig {
     public static recaptchaSiteKey = "6Ld7B5wgAAAAANF5s1Z4tJfhWPhEoDNSyM7bOfNO";
-    public static isEmulatorMode = true;
+    public static isEmulatorMode = false;
     public static isDebug = false;
     public static isFakeSubmit = false;
 }
