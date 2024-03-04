baeb24f46e517d305749ca5d8c6c35fbd1b0a326
Krishan Bandara <34151165+krishanb4@users.noreply.github.com>
2024-03-04 12:57:11 +0530

    Refactor SecondPage component

--- src/components/pages/SecondPage.tsx
+++ src/components/pages/SecondPage.tsx
@@ -1,46 +1,45 @@
-import React, {useEffect} from "react";
-import '../../styles/booking-style.css';
-import '../../styles/second-page.scss';
+import React, { useEffect } from "react";
+import "../../styles/booking-style.css";
+import "../../styles/second-page.scss";
 import NoteBanner from "../banners/NoteBanner";
 import RecaptchaItem from "../items/RecaptchaItem";
-import {MainNavbar} from "../banners/MainNavbar";
-import {PriceBanner} from "../banners/PriceBanner";
-import {BusyOverlay} from "../BusyOverlay";
-import {useService} from "react-service-locator";
-import {ReservationService} from "../../services/reservation-service";
-import {useNavigate} from "react-router-dom";
+import { MainNavbar } from "../banners/MainNavbar";
+import { PriceBanner } from "../banners/PriceBanner";
+import { BusyOverlay } from "../BusyOverlay";
+import { useService } from "react-service-locator";
+import { ReservationService } from "../../services/reservation-service";
+import { useNavigate } from "react-router-dom";
 
 export const SecondPage = (props: any) => {
-    const reservationService = useService(ReservationService);
-    const navigate = useNavigate();
-    useEffect(() => {
-        if (!reservationService.hasHomeData) {
-            navigate('/');
-        }
-    }, [])
-    return (
-        <div className={'second-page'}>
-            <section className="nav-bar-main">
-                <section className="nav-bar-main">
-                    <MainNavbar src={require('../../assets/logos/ppt-mini-logo.png')}/>
-                </section>
-            </section>
+  const reservationService = useService(ReservationService);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!reservationService.hasHomeData) {
+      navigate("/");
+    }
+  }, []);
+  return (
+    <div className={"second-page"}>
+      <section className="nav-bar-main">
+        <section className="nav-bar-main">
+          <MainNavbar src={require("../../assets/logos/ppt-mini-logo.png")} />
+        </section>
+      </section>
 
-            <section className="forms-main">
-                <NoteBanner/>
-                <div className="container">
-                    {props.children}
-                </div>
-            </section>
+      <section className="forms-main">
+        <NoteBanner />
+        <div className="container">{props.children}</div>
+      </section>
 
-            <BusyOverlay isBusy={reservationService.state.isSubmitting} text={'Submitting Reservation'}>
-                <section className="travel-fare-banner text-center fare-section">
-                    <RecaptchaItem/>
-                    <PriceBanner/>
-                </section>
-            </BusyOverlay>
-        </div>
-    );
-
-}
-
+      <BusyOverlay
+        isBusy={reservationService.state.isSubmitting}
+        text={"Submitting Reservation"}
+      >
+        <section className="travel-fare-banner text-center fare-section">
+          <RecaptchaItem />
+          <PriceBanner />
+        </section>
+      </BusyOverlay>
+    </div>
+  );
+};
